e839fbce3f00eb8a0c0e386f05d48a149db09ece
GOH23 <padadamlpd@gmail.com>
2023-03-13 18:44:42 +0300

    Changed

--- src/Components/CardAm.js
+++ src/Components/CardAm.js
@@ -2,33 +2,45 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-
+import { motion } from 'framer-motion'
 import { CardActionArea, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { StarForOneCard } from './StarForOneCard';
 export function CardAm(props) {
-    return (<>
-        {props.items.map(el => (<Grid key={el._id} item><Card  sx={{ maxWidth: 290 }}>
-            <Link to={`/anime/${el._id}`} className='text-decoration-none'>
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="400"
-                        image={el.imageFontUrl}
-                        alt='Rfhnbyrf' />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div" className='mb-0 text-dark text-center'>
-                            {el.title} <br/>
-                        </Typography>
-                        <Typography gutterBottom variant="h5" component="div" className='mb-0 text-dark text-end'>
-                            <StarForOneCard starrate={el.starsratings}/>
-                        </Typography>
-                    </CardContent>
-                    
-                </CardActionArea>
-            </Link>
+    const itemVariants = {
+        initial: {
+            opacity: 0,
+            x: -100
 
-        </Card></Grid>))}
+        },
+        anim: custom => ({ opacity: 1, x: 0, transition: { delay: custom * 0.25 } })
+    };
+    return (<>
+        {props.items.map((el ,ind)=> (<Grid key={el._id} item>
+            <motion.div variants={itemVariants} initial="initial" custom={ind} whileInView="anim" viewport={{once: true}} >
+                <Card sx={{ maxWidth: 290 }}>
+                    <Link to={`/anime/${el._id}`} className='text-decoration-none'>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                height="400"
+                                image={el.imageFontUrl}
+                                alt='Rfhnbyrf' />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div" className='mb-0 text-dark text-center'>
+                                    {el.title} <br />
+                                </Typography>
+                                <Typography gutterBottom variant="h5" component="div" className='mb-0 text-dark text-end'>
+                                    <StarForOneCard starrate={el.starsratings} />
+                                </Typography>
+                            </CardContent>
+
+                        </CardActionArea>
+                    </Link>
+
+                </Card>
+            </motion.div>
+        </Grid>))}
     </>)
 
 }
